fbbb1840588b128ee2b1ebe0dd0c08d19ccdd679
Rajat Kumar <rajat@tyloones.com>
2024-06-11 12:05:27 +0530

    main commit

--- client/src/store/Store.js
+++
@@ -1,9 +0,0 @@
-// src/store.js
-import { configureStore } from '@reduxjs/toolkit';
-import chatReducer from './chatSlice';
-
-export const store = configureStore({
-  reducer: {
-    chat: chatReducer,
-  },
-});
